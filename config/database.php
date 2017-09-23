8afec4e33a289a00c65125877184914fc0fcd560
juanpablo-degusta <bikE!vodka31>
2017-09-23 11:41:42 -0500

    First commit

---
+++ config/database.php
@@ -0,0 +1,7 @@
+<?php
+$dbhost = 'localhost';
+$dbname = 'notifications';
+$dbuser = 'juan';
+$dbpass = 'l6ozfn3-2';
+
+$link = @mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
---
+++ config/pusher.php
@@ -0,0 +1,12 @@
+<?php
+  $options = array(
+    'cluster' => 'us2',
+    'encrypted' => true
+  );
+  $pusher = new Pusher\Pusher(
+    '017d6e0aabd7ee028cbe',
+    '07e7fda20ff76ac90e1f',
+    '403834',
+    $options
+  );
+
